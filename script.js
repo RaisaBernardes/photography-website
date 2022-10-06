bbe64b6017852a9ead74360b6307aabd7daf4815
Raisa Bernardes <raisa.rmro@gmail.com>
2022-10-06 16:25:24 -0300

    navigation section done

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ $(document).ready(function() {
     $(".nav-button").on("click", function(){
         $(".nav-button").toggleClass("change");
     });
+
+    $(window).scroll(function(){ /*indent a little the navbar when the user scrolls*/
+        let position = $(this).scrollTop();
+        if(position >= 200) {
+            $('.nav-menu').addClass('custom-navbar')
+        } else {
+            $('.nav-menu').removeClass('custom-navbar')
+        }
+    })
 });
