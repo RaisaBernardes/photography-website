754be52195ab6701e9fcb39eb5e931bdf5f53bce
Raisa Bernardes <raisa.rmro@gmail.com>
2022-10-24 14:57:50 -0300

    pricing section done

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         $(".nav-button").toggleClass("change");
     });
 
-    $(window).scroll(function(){ /*indent a little the navbar when the user scrolls*/
+    $(window).scroll(function(){ /*indent the navbar when the user scrolls*/
         let position = $(this).scrollTop();
         if(position >= 200) {
             $('.nav-menu').addClass('custom-navbar')
@@ -35,5 +35,19 @@ $(document).ready(function() {
 
     $('.gallery-list-item').click(function(){
         $(this).addClass('active-item').siblings().removeClass('active-item');
+    })
+
+
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position >= 4300){
+            $('.card-1').addClass('moveFromLeft');
+            $('.card-2').addClass('moveFromBottom');
+            $('.card-3').addClass('moveFromRight');
+        }else{
+            $('.card-1').removeClass('moveFromLeft');
+            $('.card-2').removeClass('moveFromBottom');
+            $('.card-3').removeClass('moveFromRight');
+        }
     })
 });
